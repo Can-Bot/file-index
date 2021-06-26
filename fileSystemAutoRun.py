b782a183b1d797d02a5c6070adbc309a72a2f76c
CanBot02 <63718922+Can-Bot@users.noreply.github.com>
2021-06-26 14:51:13 +0000

    added descriptions on how the program works

--- fileSystemAutoRun.py
+++ fileSystemAutoRun.py
@@ -3,25 +3,27 @@ from PIL import Image
 
 
 #These are the files that the program will create/use
-fileListName = "fileList.txt"
-jsonFile = "fileTableAutoRun.json"
-dictFileName = "dictFileAutoRun.txt"
+fileListName = "fileList.txt" #list of files to be indexed
+jsonFile = "fileTableAutoRun.json" #json version of the file table
+dictFileName = "dictFileAutoRun.txt" #txt file of the dictionary
 
+#if the json file is present, it is loaded as the file table.
 try:
 	fileTable = json.load(open(jsonFile))
 except:
+	# A new dictionary is used as the file table otherwise.
 	fileTable = {}
 
 
 #class for each image UNUSED
-class ImageEntry:
-	def __init__(name,Extention,Width,Height,bAlen,bArray):
-		self.name = name
-		self.Extention = Extention
-		self.Width = Width
-		self.Height = Height
-		self.bAlen = bAlen
-		self.bArray = bArray
+#class ImageEntry:
+#	def __init__(name,Extention,Width,Height,bAlen,bArray):
+#		self.name = name
+#		self.Extention = Extention
+#		self.Width = Width
+#		self.Height = Height
+#		self.bAlen = bAlen
+#		self.bArray = bArray
 
 
 # Takes the list of files in the module and idexes them
@@ -41,21 +43,26 @@ def fileTableAdd(fileTable):
 			# extention
           fileNameExt = fileName.split(".")[1]
 
+			# old method of getting the bytearray
 			# gets byteArray
           #buf = io.BytesIO()
           #imRGB = im.convert("RGB")          
           #imRGB.save(buf, format=fileNameExt)
           #imByteFor = buf.getvalue()
 
+			# new method of getting bytearray
           pixel_list = list(im.getdata())
           imByteArr = ""
           imIntArr = []
           
+					# loop that separates the pixel list into rgb values and converts to rgb565 format
           for pix in pixel_list:
+						#separates to rgb values and converts into hex
             r = (pix[0] >> 3) & 0x1F
             g = (pix[1] >> 2) & 0x3F
             b = (pix[2] >> 3) & 0x1F
 
+						# turns each value into a string and adjusts to 565 format
             rStr = (str(bin(r)).strip('0b'))
             if len(rStr) < 5 :
               rMiss = 5 - len(rStr)
@@ -71,6 +78,7 @@ def fileTableAdd(fileTable):
               bMiss = 5 - len(bStr)
               bStr = ("0" * bMiss) + bStr   
 
+						#adds the pixel values to the byte array
             imByteArr = imByteArr + (rStr + gStr + bStr)
             imIntArr.append((r,g,b))
 
@@ -83,17 +91,19 @@ def fileTableAdd(fileTable):
 
           im.close()
 
+					# Adds / adjusts the elements in the file table
           fileTable[fileName] = {"Name": str(fileNameBase),
 															"Extention": str(fileNameExt),
 															"Width": imWidth,
 															"Height": imHeight,
 															"bAlen": len(imByteArr),
-															"ByteArray": imByteArr
+															"ByteArray": bin(int(imByteArr))
 														}
 
-          binF = open("duck.bin", "wb")
-          
-          binF.write(imByteArr.encode('utf-8'))
+          # Testing for creating bin files
+					#binF = open("duck.bin", "wb")
+          #binF.write(imByteArr.encode('utf-8'))
+
       except:
         print("Something went wrong with file, ", fileName,  " check the file name given or the file itself")
 
@@ -107,8 +117,6 @@ def fileTableJSON(fileTable):
     jsonDict = {}
     with open(jsonFile, "w") as outfile:
         jsonDict = fileTable
-#        for fileKey in jsonDict.keys():
-#            fileTable[fileKey].pop("File")
         json.dump(jsonDict, outfile, indent=4)
     outfile.close()
     return
@@ -120,7 +128,7 @@ def createDictFile(exDict):
     newFile.close()
     return
 
-
+#calls each functions
 fileTableAdd(fileTable)
 fileTableJSON(fileTable)
 createDictFile(fileTable)
--- test.py
+++
@@ -1,7 +0,0 @@
-binF = open("duck.bin", "wb")
-
-test = "this is a test"
-
-testBytes = bytearray(test.encode('utf-8'))
-
-binF.write(testBytes)
